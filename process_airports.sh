64ef39ccaebcedb95ba0878f1ba1459b0345cac2
Daniel Prosser <dpross1100@msn.com>
2020-04-23 12:54:19 -0400

    Initial commit

---
+++ process_airports.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+. functions/airports.sh
+
+echo "Working on block 1 of 17..."
+process_airports -79.500000 -78.000000 38.700000 39.740000
+echo "Working on block 2 of 17..."
+process_airports -80.500000 -79.500000 37.550000 38.600000
+echo "Working on block 3 of 17..."
+process_airports -79.500000 -78.000000 37.550000 38.700000
+echo "Working on block 4 of 17..."
+process_airports -83.000000 -80.500000 36.500000 37.450000
+echo "Working on block 5 of 17..."
+process_airports -83.625000 -83.000000 36.500000 36.950000
+echo "Working on block 6 of 17..."
+process_airports -80.500000 -79.500000 36.500000 37.550000
+echo "Working on block 7 of 17..."
+process_airports -79.500000 -78.000000 36.500000 37.550000
+echo "Working on block 8 of 17..."
+process_airports -78.000000 -77.000000 36.500000 38.000000
+echo "Working on block 9 of 17..."
+process_airports -77.000000 -75.000000 36.500000 38.000000
+echo "Working on block 10 of 17..."
+process_airports -78.000000 -77.000000 38.000000 38.700000
+echo "Working on block 11 of 17..."
+process_airports -77.500000 -76.750000 38.700000 39.100000
+echo "Working on block 12 of 17..."
+process_airports -77.000000 -74.875000 38.000000 38.700000
+echo "Working on block 13 of 17..."
+process_airports -76.750000 -75.050000 38.700000 39.100000
+echo "Working on block 14 of 17..."
+process_airports -76.950000 -76.300000 39.100000 39.740000
+echo "Working on block 15 of 17..."
+process_airports -76.300000 -75.390000 39.100000 39.740000
+echo "Working on block 16 of 17..."
+process_airports -78.000000 -77.500000 38.700000 39.740000
+echo "Working on block 17 of 17..."
+process_airports -77.500000 -76.950000 39.100000 39.740000
---
+++ tg-construct.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+. functions/tg-construct.sh
+
+echo "Working on block 1 of 17..."
+construct -79.500000 -78.000000 38.700000 39.615000
+echo "Working on block 2 of 17..."
+construct -80.500000 -79.500000 37.550000 38.475000
+echo "Working on block 3 of 17..."
+construct -79.500000 -78.000000 37.550000 38.700000
+echo "Working on block 4 of 17..."
+construct -83.000000 -80.500000 36.500000 37.325000
+echo "Working on block 5 of 17..."
+construct -83.625000 -83.000000 36.500000 36.825000
+echo "Working on block 6 of 17..."
+construct -80.500000 -79.500000 36.500000 37.550000
+echo "Working on block 7 of 17..."
+construct -79.500000 -78.000000 36.500000 37.550000
+echo "Working on block 8 of 17..."
+construct -78.000000 -77.000000 36.500000 38.000000
+echo "Working on block 9 of 17..."
+construct -77.000000 -75.000000 36.500000 38.000000
+echo "Working on block 10 of 17..."
+construct -78.000000 -77.000000 38.000000 38.700000
+echo "Working on block 11 of 17..."
+construct -77.500000 -76.750000 38.700000 39.100000
+echo "Working on block 12 of 17..."
+construct -77.000000 -74.875000 38.000000 38.700000
+echo "Working on block 13 of 17..."
+construct -76.750000 -75.050000 38.700000 39.100000
+echo "Working on block 14 of 17..."
+construct -76.950000 -76.300000 39.100000 39.615000
+echo "Working on block 15 of 17..."
+construct -76.300000 -75.390000 39.100000 39.615000
+echo "Working on block 16 of 17..."
+construct -78.000000 -77.500000 38.700000 39.615000
+echo "Working on block 17 of 17..."
+construct -77.500000 -76.950000 39.100000 39.615000
